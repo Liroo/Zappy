94ebfd3eb64fd240303683c3c7bd0d994649c678
Liroo <pierre-monge@hotmail.fr>
2017-06-23 01:33:14 +0200

    🎨  Makefile color fix on linux

--- mk/colors.mk
+++ mk/colors.mk
@@ -19,15 +19,15 @@ WHITE_R=
 else
 
 # Colors definitions
-CLEAR= \e[0m
+CLEAR= \033[0m
 
 # Regular
-BLACK_R= \e[0;30m
-RED_R= \e[0;31m
-GREEN_R= \e[0;32m
-YELLOW_R= \e[0;33m
-BLUE_R=	\e[0;34m
-PURPLE_R= \e[0;35m
-CYAN_R=	\e[0;36m
-WHITE_R= \e[0;37m
+BLACK_R= \033[0;30m
+RED_R= \033[0;31m
+GREEN_R= \033[0;32m
+YELLOW_R= \033[0;33m
+BLUE_R= \033[0;34m
+PURPLE_R= \033[0;35m
+CYAN_R= \033[0;36m
+WHITE_R= \033[0;37m
 endif
