0ca1b8536d0b7c7f944fbbe63f34438cc623b52d
andre <maxime.andre@epitech.eu>
2017-06-28 01:57:49 +0200

    suite ia

--- ai/src/Ai.cpp
+++ ai/src/Ai.cpp
@@ -10,6 +10,101 @@
 
 # include "Ai.h"
 
-Ai::Ai() : level(1) {}
+Ai::Ai() : _level(1), _life(1260), _action({Ai::ActionType::UNKNOWN, ""}), _dir(Ai::Direction::UNKNOWN) {}
 
 Ai::~Ai() {}
+
+const int &Ai::getLevel() const {
+  return _level;
+}
+
+void Ai::setLevel(const int &var) {
+  _level = var;
+}
+
+const Inventory &Ai::getBag() const {
+  return _bag;
+}
+
+void Ai::setBag(const Inventory &var) {
+  _bag = var;
+}
+
+const std::vector<Inventory> &Ai::getViewMaterial() const {
+  return _viewMaterial;
+}
+
+void Ai::setViewMaterial(const std::vector<Inventory> &var) {
+  _viewMaterial = var;
+}
+
+const int &Ai::getLife() const {
+  return _life;
+}
+
+void Ai::setLife(const int &var) {
+  _life = var;
+}
+
+const std::pair<Ai::ActionType, std::string> &Ai::getAction() const {
+  return _action;
+}
+
+void Ai::setAction(const std::pair<Ai::ActionType, std::string> &var) {
+  _action = var;
+}
+
+void Ai::forward() {
+  std::cout << "forward" << std::endl;
+  _life--;
+}
+
+void Ai::right() {
+  std::cout << "right" << std::endl;
+  _life--;
+}
+
+void Ai::left() {
+  std::cout << "left" << std::endl;
+  _life--;
+}
+
+void Ai::look() {
+  std::cout << "look" << std::endl;
+  _life--;
+}
+
+void Ai::inventory() {
+  std::cout << "inventory" << std::endl;
+  _life--;
+}
+
+void Ai::broadcast(std::string const &var) {
+  std::cout << "broadcast" << var << std::endl;
+  _life--;
+}
+
+void Ai::fork() {
+  std::cout << "fork" << std::endl;
+  _life--;
+}
+
+void Ai::eject() {
+  std::cout << "eject" << std::endl;
+  _life--;
+}
+
+void Ai::take(std::string const &var) {
+  std::cout << "take" << var << std::endl;
+  _life--;
+}
+
+void Ai::set(std::string const &var) {
+  std::cout << "set" << var << std::endl;
+  _life--;
+}
+
+void Ai::incantation() {
+  std::cout << "incantation" << std::endl;
+  _life--;
+}
--- ai/src/Inventory.cpp
+++ ai/src/Inventory.cpp
@@ -10,6 +10,62 @@
 
 # include "Inventory.h"
 
-Inventory::Inventory() : linemate(0), deraumere(0), sibur(0), mendiane(0), phiras(0), thystame(0) {}
+Inventory::Inventory() : _food(0), _linemate(0), _deraumere(0), _sibur(0), _mendiane(0), _phiras(0), _thystame(0) {}
 
 Inventory::~Inventory() {}
+
+const int &Inventory::getLinemate() const {
+  return _linemate;
+}
+
+void Inventory::setLinemate(const int &mat) {
+  _linemate = mat;
+}
+
+const int &Inventory::getDeraumere() const {
+  return _deraumere;
+}
+
+void Inventory::setDeraumere(const int &mat) {
+  _deraumere = mat;
+}
+
+const int &Inventory::getSibur() const {
+    return _sibur;
+}
+
+void Inventory::setSibur(const int &mat) {
+  _sibur = mat;
+}
+
+const int &Inventory::getMendiane() const {
+  return _mendiane;
+}
+
+void Inventory::setMendiane(const int &mat) {
+  _mendiane = mat;
+}
+
+const int &Inventory::getPhiras() const {
+  return _phiras;
+}
+
+void Inventory::setPhiras(const int &mat) {
+  _phiras = mat;
+}
+
+const int &Inventory::getThystame() const {
+  return _thystame;
+}
+
+void Inventory::setThystame(const int &mat) {
+  _thystame = mat;
+}
+
+const int &Inventory::getFood() const {
+  return _food;
+}
+
+void Inventory::setFood(const int &mat) {
+  _food = mat;
+}
